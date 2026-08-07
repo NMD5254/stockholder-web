7a4a613caf7a4076c8b297d32b2c8924f4907d64
aokit1946-lang <aoki.t1946@gmail.com>
2026-08-07 15:44:33 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+async function login() {
+
+    const stockNo =
+        document.getElementById("stockNo").value;
+
+    const authCode =
+        document.getElementById("authCode").value;
+
+    const response = await fetch(
+        "https://func-stockholder-auth22-ebg8c2cqb3eqdthu.japanwest-01.azurewebsites.net/api/login",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                stockNo: stockNo,
+                authCode: authCode
+            })
+        }
+    );
+
+    const result = await response.json();
+
+    if (result.success) {
+
+        window.location.href =
+            result.pdfUrl;
+
+    } else {
+
+        document.getElementById("message")
+            .innerText =
+            result.message || "ログインに失敗しました";
+
+    }
+}
